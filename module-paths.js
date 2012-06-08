66649f202cfcdb6a728ca13221fcb2fb011ebe53
Petro Salema <p.salema@gentics.com>
2012-06-08 15:28:19 +0200

    Adding optional root property in SETTINGS object, and reading it in determining MODULE_PATHS strings.

--- module-paths.js
+++ module-paths.js
@@ -1,8 +1,10 @@
-var MODULE_PATHS = MODULE_PATHS || {};
-MODULE_PATHS['jquery-layout'] = 'vendor/jquery.layout';     // Mutates jquery
-MODULE_PATHS['jstree'] = 'vendor/jquery.jstree';            // Mutates jquery
-MODULE_PATHS['jqgrid'] = 'vendor/jquery.jqgrid';            // Mutates jquery
-MODULE_PATHS['jqgrid-locale-en'] = 'vendor/grid.locale.en'; // Mutates jqgrid
-MODULE_PATHS['jqgrid-locale-de'] = 'vendor/grid.locale.de'; // Mutates jqgrid
-MODULE_PATHS['repository-browser-i18n-en'] = 'nls/en/i18n';
-MODULE_PATHS['repository-browser-i18n-de'] = 'nls/de/i18n';
+var root = (typeof SETTINGS !== 'undefined' && SETTINGS.root) ? SETTINGS.root : '';
+var MODULE_PATHS = (typeof MODULE_PATHS !== 'undefined') ? MODULE_PATHS : {};
+
+MODULE_PATHS['jquery-layout'] = root + 'vendor/jquery.layout';     // Mutates jquery
+MODULE_PATHS['jstree'] = root + 'vendor/jquery.jstree';            // Mutates jquery
+MODULE_PATHS['jqgrid'] = root + 'vendor/jquery.jqgrid';            // Mutates jquery
+MODULE_PATHS['jqgrid-locale-en'] = root + 'vendor/grid.locale.en'; // Mutates jqgrid
+MODULE_PATHS['jqgrid-locale-de'] = root + 'vendor/grid.locale.de'; // Mutates jqgrid
+MODULE_PATHS['repository-browser-i18n-en'] = root + 'nls/en/i18n';
+MODULE_PATHS['repository-browser-i18n-de'] = root + 'nls/de/i18n';
