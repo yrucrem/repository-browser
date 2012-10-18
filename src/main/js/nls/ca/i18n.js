43c63e45c247d81c8f88a407af19a4c2b4d261a8
Johannes Schüth <j.schueth@gentics.com>
2012-10-18 16:22:24 +0200

    Added i18n translations that were added to aloha repository some time ago. We'll now also include the german i18n translations within the build process.

---
+++ src/main/js/nls/ca/i18n.js
@@ -0,0 +1,21 @@
+define('repository-browser-i18n-ca', [], function () {
+	'use strict';
+	return {
+		"jgrid.locale": "ca",
+		"button.switch-metaview.tooltip": "Canvia entre la visualització «meta» i «normal»",
+		"Browsing": "S\'està navegant",
+		"Searching for": "S\'està cercant per",
+		"in": "en",
+		"Viewing": "S\'està visualitzant",
+		"numerous": "molts",
+		"of": "de",
+		"Close": "Tanca",
+		"Open": "Obre",
+		"Search": "Cerca",
+		"Name": "Nom",
+		"URL": "URL",
+		"Preview": "Previsualitza",
+		"No records to view": "No hi ha cap registre per veure",
+		"Input search text...": "Text a cercar..."
+	};
+});
---
+++ src/main/js/nls/ru/i18n.js
@@ -0,0 +1,21 @@
+define('repository-browser-i18n-ru', [], function () {
+	'use strict';
+	return {
+		"jgrid.locale": "en",
+		"button.switch-metaview.tooltip": "Переключиться между мета- и нормальным видом",
+		"Browsing": "Поиск с просмотром",
+		"Searching for": "Искать",
+		"in": "в",
+		"Viewing": "Просмотр",
+		"numerous": "нумерованный",
+		"of": "из",
+		"Close": "Закрыть",
+		"Open": "Открыть",
+		"Search": "Поиск",
+		"Name": "Имя",
+		"URL": "URL",
+		"Preview": "Предпросмотр",
+		"No records to view": "Нет записей для просмотра",
+		"Input search text...": "Введите текст поиска..."
+	};
+});
